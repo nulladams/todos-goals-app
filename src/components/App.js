4c2d261c45f90d7aedb270f858aaaf8071f19f4b
Leonardo Adams <leonardoadams@gmail.com>
2019-08-25 16:08:47 -0300

    Add components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import {
+  handleInitialData
+} from '../actions/shared'
 
-function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
+class App extends React.Component {
+  componentDidMount() {
+      const { dispatch } = this.props
+
+      dispatch(handleInitialData())
+
+  //    store.subscribe(() => this.forceUpdate())
+  }
+  render() {
+      if (this.props.loading === true) {
+          return <h3>Loading</h3>
+      }
+
+      return (
+          <div>
+              <ConnectedTodos />
+              <ConnectedGoals />
+          </div>
+      )
+  }
 }
 
-export default App;
+export default connect((state) => ({
+  todos: state.todos
+}))(Todos)
+
